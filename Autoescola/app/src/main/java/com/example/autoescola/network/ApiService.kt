0726bceab3be100d14b96c65de73c0b37abc9446
a18marcastru <a18marcastru@inspedralbes.cat>
2024-10-14 11:57:43 +0200

    Modificaciones de la aplicacion

--- Autoescola/app/src/main/java/com/example/autoescola/network/ApiService.kt
+++ Autoescola/app/src/main/java/com/example/autoescola/network/ApiService.kt
@@ -11,9 +11,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 
 object RetrofitInstance {
-    // private const val BASE_URL = "http://dam.inspedralbes.cat:20999/" // Reemplaza <tu_ip> con tu IP local o localhost
-
-    private const val BASE_URL = "http://10.0.2.2:20000/"
+    private const val BASE_URL = "http://dam.inspedralbes.cat:20999/"
 
     private val retrofit by lazy {
         Retrofit.Builder()
